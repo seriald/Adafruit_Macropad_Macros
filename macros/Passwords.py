c47484c4e534b5be4c61808ef68973fe04c4a740
Robert Fleming <robert.fleming@vitori.org>
2022-08-13 20:08:05 -0400

    Applied changes to Passwords file

--- macros/Passwords.py
+++ macros/Passwords.py
@@ -12,21 +12,21 @@ app = {                       # REQUIRED dict, must be named 'app'
     'macros' : [              # List of button macros...
         # COLOR    LABEL    KEY SEQUENCE
         # 1st row ----------
-        (0x004000, 'Work', ['xxxx', Keycode.ENTER]),
+        (0x004000, 'Work', ['xxxxxx', Keycode.ENTER]),
         (0x000000, '', ''),
-        (0x000040, 'Admin', ['xxxx', Keycode.ENTER]),
+        (0x004000, 'Admin', ['xxxxx', Keycode.ENTER]),
         # 2nd row ----------
-        (0x000000, '', ''),
+        (0x004000, 'User', 'xxxxxx'),
         (0x2d53b3, 'Enter', [Keycode.ENTER]),
-        (0x000000, '', ''),
+        (0x004000, 'User A', 'xxxxx'),
         # 3rd row ----------
         (0x000000, '', ''),
         (0x000000, '', ''),
         (0x000000, '', ''),
         # 4th row ----------
+        (0x83232, 'ADM', ['xxxxxx', Keycode.ENTER]),
         (0x000000, '', ''),
-        (0x000000, '', ''),
-        (0x83232, 'Home ', ['xxxx', Keycode.ENTER]),
+        (0x83232, 'Home ', ['xxxxx', Keycode.ENTER]),
         # Encoder button ---
         (0x000000, '', [Keycode.CONTROL, Keycode.ALT, 'S']) # Save for web
     ]
